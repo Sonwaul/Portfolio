a4d46606f4755982a331fe4581dbf005fa22ee1b
Sonwaul <infeltaelliot@gmail.com>
2024-02-28 19:44:20 +0100

    on commence le porfolio

---
+++ portfolio-next-js/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Portfolio Infelta Elliot © 2024",
+  description: "Portfolio permettant de présenter mes compétences, mes expériences, mes études ainsi que mes passions et mes réalisations.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
